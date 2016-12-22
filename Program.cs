d046f2f34c123941810f7b1ad1f7853165fe2f00
Filipe Silva <f.silva@coolblue.nl>
2016-12-22 15:33:59 +0100

    Initial (and probably only) commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConfigLinkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Test setting: {ConfigurationManager.AppSettings["TestSetting"]}");
+            Console.ReadLine();
+        }
+    }
+}
